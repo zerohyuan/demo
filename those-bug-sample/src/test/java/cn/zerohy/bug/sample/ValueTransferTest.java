1d754288680445478f2dcc21a795e96b007af329
zerohyuan <zerohyuan@163.com>
2019-07-23 21:25:27 +0800

    完成部分内容

--- those-bug-sample/src/test/java/cn/zerohy/bug/sample/ValueTransferTest.java
+++ those-bug-sample/src/test/java/cn/zerohy/bug/sample/ValueTransferTest.java
@@ -1,9 +1,23 @@
 package cn.zerohy.bug.sample;
 
+import org.junit.Test;
+
 /**
  * @Author hyuan
  * @Date 2019-07-06 16:23
  * @Description Java中的值传递
  */
 public class ValueTransferTest {
+
+    @Test
+    public void strTest() {
+        String str = "ABC";
+        changeStr(str);
+        System.out.println(str);
+    }
+
+    private void changeStr(String str) {
+        String replace = str.replace('B', 'D');
+        System.out.println(replace);
+    }
 }
